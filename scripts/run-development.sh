a156d0d520fabee2bbf66f066ba778502fccc852
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-09-16 18:43:01 +0200

    Docker setup refactor

--- scripts/run-development.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git submodule foreach npm install
-git submodule foreach npm run build
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.mongo.yml up --build
--- scripts/run-production-rpi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -f docker-compose.yml -f docker-compose.prod.rpi.yml up -d
