2d761174d66e6353e33e099cd75e70da947d8df0
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-09-22 17:27:08 +0200

    Added Travis stages

--- scripts/ci-docker.sh
+++ scripts/ci-docker.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 printenv > .env
-cd packages/back
-npm install
-npm run build
-cd -
 docker-compose -f docker-compose.test.yml up -d --build
 docker ps -a
--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-cd packages/back
-npm install
-npm run build
-cd -
 docker-compose -f docker-compose.test.yml up -d --build
 docker ps -a
 npm test
