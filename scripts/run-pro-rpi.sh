705e0f73dfae3a7e8b5235f25c9f3f9850045d45
mmontes11 <martin11lrx@gmail.com>
2019-10-09 19:41:17 +0000

    Updated nginx conf

--- scripts/run-pro-rpi.sh
+++ scripts/run-pro-rpi.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 npm run build
-docker-compose -f docker-compose.yml -f docker-compose.pro.rpi.yml -f docker-compose.services.yml  up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.pro.rpi.yml -f docker-compose.services.yml up -d --build
